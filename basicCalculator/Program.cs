ae63e92edc899eeae7830aed070d12f4161baf7b
davidaclawson <davidaclawson@hotmail.com>
2025-01-18 20:22:26 +0000

    Changes made to allow decimal numbers instead of just whole integers. Also fixed a bug that used the wrong operater when using lower-case equivalent of the necessary characters.

--- basicCalculator/Program.cs
+++ basicCalculator/Program.cs
@@ -10,26 +10,26 @@ namespace basicCalculator
     {
         static void Main(string[] args)
         {
-            int number1;
-            int number2;
+            float number1;
+            float number2;
 
-            int result;
+            float result;
 
             string answer;
 
             Console.WriteLine("Hello, welcome to my calculator program!");
             Console.WriteLine("Please enter your first number: ");
 
-            number1 = Convert.ToInt32(Console.ReadLine());
+            number1 = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Please enter your second number: ");
 
-            number2 = Convert.ToInt32(Console.ReadLine());
+            number2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine("What type of operation would you like to do?");
             Console.WriteLine("Please enter A for addition, S for subtraction, M for multiplication, or any other key for division");
-            
-            answer = Console.ReadLine();
+
+            answer = Console.ReadLine().ToUpper();
 
             if (answer == "A")
             {
@@ -45,11 +45,18 @@ namespace basicCalculator
             }
             else
             {
-                result = number1 / number2;
+                result = number2 == 0? float.NaN : number1 / number2;
             }
 
+            if(float.IsNaN(result))
+            {
+                Console.WriteLine("Cannot divide by 0!");
+            }
+            else
+            { 
+                Console.WriteLine("The answer is: " + result); 
+            }
 
-            Console.WriteLine("The answer is: " + result);
             Console.WriteLine("Thank you for using my calculator!"); ;
 
             Console.ReadKey();
